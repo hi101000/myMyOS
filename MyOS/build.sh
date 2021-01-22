cabf81a08f566c94359fd67ae4c762e1e3eb74cc
Ishan <piklenews@gmail.com>
2021-01-22 09:06:30 -0500

    edit build.sh

--- MyOS/build.sh
+++ MyOS/build.sh
@@ -4,4 +4,7 @@ ld -m elf_i386 -T link.ld -o kernel.bin ksm.o kc.o
 qemu-system-i386 -kernel kernel.bin
 cp kernel.bin MyOS/boot/kernel.bin
 grub-mkrescue -o myos.iso MyOS/
+git add .
+git commit
+git push
 #clear
