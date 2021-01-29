4e78b0b752aef41d58e06f3f202df15cca0246d3
Ishan Roychowdhury <ishan@localhost.localdomain>
2021-01-29 10:26:27 -0500

    well wouldn't you like to know

--- MyOS/dict.h
+++ MyOS/dict.h
@@ -1,3 +1,24 @@
-struct Dictionary{
-    
+#ifndef _DICT_H
+#define _DICT_H 1
+#include "lib/stdint.h"
+class Dictionary{
+public:
+   uint16_t key;
+   uint16_t val;
+   Dictionary(uint16_t dictkey, uint16_t value){
+       key=dictkey;
+       val=value;
+   }
 };
+
+typedef Dictionary* dict;
+
+void addItem(dict d, int idx, uint16_t key, uint16_t val){
+    d[idx]=Dictionary(key, val);
+}
+
+void rmItem(dict d, int idx){
+    d[idx]=Dictionary('\0', '\0');
+}
+
+#endif
