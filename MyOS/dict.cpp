c3ffd57061dce794e5cebfaca8b9eba05751189e
Ishan Roychowdhury <ishan@localhost.localdomain>
2021-01-30 21:56:03 -0500

    well wouldn't you like to know

--- MyOS/dict.cpp
+++ MyOS/dict.cpp
@@ -1,1 +1,26 @@
 #include "dict.h"
+
+#include "lib/stdint.h"
+class Dictionary{
+public:
+   uint16_t* key;
+   uint16_t* val;
+   Dictionary(uint16_t* dictkey, uint16_t* value){
+       key=dictkey;
+       val=value;
+   }
+};
+
+typedef Dictionary* dict;
+
+void addItem(dict d, int idx, uint16_t* key, uint16_t* val){
+    d[idx]=Dictionary(key, val);
+}
+
+void rmItem(dict d, int idx){
+    d[idx]=Dictionary((uint16_t*)"\0", (uint16_t*)"\0");
+}
+
+void changeItem(dict d, int idx, uint16_t* key, uint16_t* val){
+    d[idx]=Dictionary(key, val);
+}
--- MyOS/dict.h
+++ MyOS/dict.h
@@ -1,28 +1,14 @@
 #ifndef _DICT_H
 #define _DICT_H 1
 #include "lib/stdint.h"
-class Dictionary{
-public:
-   uint16_t* key;
-   uint16_t* val;
-   Dictionary(uint16_t* dictkey, uint16_t* value){
-       key=dictkey;
-       val=value;
-   }
-};
+class Dictionary;
 
 typedef Dictionary* dict;
 
-void addItem(dict d, int idx, uint16_t* key, uint16_t* val){
-    d[idx]=Dictionary(key, val);
-}
+void addItem(dict d, int idx, uint16_t* key, uint16_t* val);
 
-void rmItem(dict d, int idx){
-    d[idx]=Dictionary((uint16_t*)"\0", (uint16_t*)"\0");
-}
+void rmItem(dict d, int idx);
 
-void changeItem(dict d, int idx, uint16_t* key, uint16_t* val){
-    d[idx]=Dictionary(key, val);
-}
+void changeItem(dict d, int idx, uint16_t* key, uint16_t* val);
 
 #endif
