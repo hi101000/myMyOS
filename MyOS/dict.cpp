cea60b3f32e353ce9f967d388bab98a9215767d9
hi101000 <piklenews@gmail.com>
2021-02-05 19:21:32 -0500

    well wouldn't you like to know

--- MyOS/dict.cpp
+++ MyOS/dict.cpp
@@ -15,8 +15,12 @@ void addItem(dict dic, int idx, uint16_t* key, uint16_t* val){
     dic.d[idx]=Dictionary(key, val);
 }
 
-void rmItem(dict dic, int idx){
-    dic.d[idx]=Dictionary((uint16_t*)"\0", (uint16_t*)"\0");
+int rmItem(dict dic, int idx){
+    Dictionary dictionary_to_test=Dictionary((uint16_t*)"\0", (uint16_t*)"\0");
+    if(dic.d[idx].key!=dictionary_to_test.key && dic.d[idx].val!=dictionary_to_test.val)
+        dic.d[idx]=Dictionary((uint16_t*)"\0", (uint16_t*)"\0");
+        return 0;
+    return idx;
 }
 
 void changeItem(dict dic, int idx, uint16_t* key, uint16_t* val){
