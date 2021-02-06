b39c45d852fef8fa6175bf836fc720a09e78a625
hi101000 <piklenews@gmail.com>
2021-02-05 19:13:16 -0500

    well wouldn't you like to know

--- MyOS/dictc.c
+++ MyOS/dictc.c
@@ -1,12 +1,12 @@
 #include "lib/stdint.h"
 typedef struct{
-    uint16_t* key;
-    uint16_t* val;
+    uint16_t key;
+    uint64_t* val;
 } Dictionary;
 
 typedef struct{Dictionary x[1000000];} dict;
 
-void addItem(dict dic, int idx, uint16_t* key, uint16_t* val){
+void addItem(dict dic, int idx, uint16_t key, uint64_t* val){
     Dictionary d;
     d.key=key;
     d.val=val;
@@ -15,12 +15,13 @@ void addItem(dict dic, int idx, uint16_t* key, uint16_t* val){
 
 void rmItem(dict dic, int idx){
     Dictionary d;
-    d.key=(uint16_t*)"\0";
-    d.val=(uint16_t*)"\0";
+    char* null="\0";
+    d.key=(uint16_t)'\0';
+    d.val=(uint64_t*)null;
     dic.x[idx]=d;
 }
 
-void changeItem(dict dic, int idx, uint16_t* key, uint16_t* val){
+void changeItem(dict dic, int idx, uint16_t key, uint64_t* val){
     Dictionary d;
     d.key=key;
     d.val=val;
